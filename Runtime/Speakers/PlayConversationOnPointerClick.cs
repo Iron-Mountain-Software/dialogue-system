0b2df84205fccda39c450f7f03a6bfe215a8ab66
ludachrismonte <ludachrismonte@gmail.com>
2023-08-22 09:34:02 -0400

    icons and play on click

---
+++ Runtime/Speakers/PlayConversationOnPointerClick.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SpellBoundAR.DialogueSystem.Speakers
+{
+    public class PlayConversationOnPointerClick : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private SpeakerController speakerController;
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (speakerController) speakerController.PlayConversation();
+        }
+
+#if UNITY_EDITOR
+        
+        private void OnValidate()
+        {
+            if (!speakerController) speakerController = GetComponentInParent<SpeakerController>();
+            if (!speakerController) Debug.LogWarning("Warning: Missing a SpeakerController!", this);
+        }
+        
+#endif
+
+    }
+}
