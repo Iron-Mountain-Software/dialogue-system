dbcab939b80e5b836402ef1ff845387f992e53d8
ludachrismonte <ludachrismonte@gmail.com>
2023-08-23 14:58:12 -0400

    testing

--- Runtime/Nodes/Conditions/PassFailConditionDialogueQueued.cs
+++ Runtime/Nodes/Conditions/PassFailConditionDialogueQueued.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace SpellBoundAR.DialogueSystem.Nodes.Conditions
 {
     public class PassFailConditionDialogueQueued : PassFailCondition
@@ -6,14 +8,25 @@ namespace SpellBoundAR.DialogueSystem.Nodes.Conditions
 
         protected override bool TestCondition(ConversationUI conversationUI)
         {
+            Debug.Log("RUNNING DIALOGUE QUEUED CHECK!");
             Conversation thisConversation = conversationUI.CurrentConversation;
+            Debug.Log("thisConversation: " + thisConversation.Name);
             foreach (Conversation testConversation in thisConversation.Speaker.Conversations)
             {
+                Debug.Log("Testing: " + testConversation);
+                Debug.Log("is active: " + testConversation.IsActive);
+                Debug.Log("is not this: " + (testConversation != thisConversation));
+                Debug.Log("is not prioritized: " + !testConversation.PrioritizeOverDefault);
                 if (testConversation
                     && testConversation.IsActive
                     && testConversation != thisConversation
-                    && !testConversation.PrioritizeOverDefault) return true;
+                    && !testConversation.PrioritizeOverDefault)
+                {
+                    Debug.Log("RETIRNING TRUE");
+                    return true;
+                }
             }
+            Debug.Log("RETIRNING FALSE");
             return false;
         }
         
