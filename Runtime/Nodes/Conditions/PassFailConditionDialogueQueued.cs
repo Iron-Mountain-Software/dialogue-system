96ac36f84c5b90e87d2edc581cabdc68fe1eda21
ludachrismonte <ludachrismonte@gmail.com>
2023-08-23 15:36:38 -0400

    bug fixes

--- Runtime/Nodes/Conditions/PassFailConditionDialogueQueued.cs
+++ Runtime/Nodes/Conditions/PassFailConditionDialogueQueued.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace SpellBoundAR.DialogueSystem.Nodes.Conditions
 {
     public class PassFailConditionDialogueQueued : PassFailCondition
@@ -8,25 +6,17 @@ namespace SpellBoundAR.DialogueSystem.Nodes.Conditions
 
         protected override bool TestCondition(ConversationUI conversationUI)
         {
-            Debug.Log("RUNNING DIALOGUE QUEUED CHECK!");
             Conversation thisConversation = conversationUI.CurrentConversation;
-            Debug.Log("thisConversation: " + thisConversation.Name);
             foreach (Conversation testConversation in thisConversation.Speaker.Conversations)
             {
-                Debug.Log("Testing: " + testConversation);
-                Debug.Log("is active: " + testConversation.IsActive);
-                Debug.Log("is not this: " + (testConversation != thisConversation));
-                Debug.Log("is not prioritized: " + !testConversation.PrioritizeOverDefault);
                 if (testConversation
                     && testConversation.IsActive
                     && testConversation != thisConversation
                     && !testConversation.PrioritizeOverDefault)
                 {
-                    Debug.Log("RETIRNING TRUE");
                     return true;
                 }
             }
-            Debug.Log("RETIRNING FALSE");
             return false;
         }
         
