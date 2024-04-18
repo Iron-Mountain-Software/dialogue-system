da80f176a8f37d1080e894deb3df98aa369d3126
ludachrismonte <ludachrismonte@gmail.com>
2024-04-18 11:34:20 -0600

    more

--- Runtime/UI/Responses/DialogueResponseBlock.cs
+++ Runtime/UI/Responses/DialogueResponseBlock.cs
@@ -8,48 +8,33 @@ namespace IronMountain.DialogueSystem.UI.Responses
 {
     public class DialogueResponseBlock : MonoBehaviour
     {
-        [Header("Settings")]
+        [SerializeField] protected DialogueResponseButton responseButtonPrefab;
+        [SerializeField] protected Transform parent;
         [SerializeField] private float destructionDelay = 1;
-        [SerializeField] private float xPadding;
-        [SerializeField] private float yPadding;
-        [SerializeField] private float spacing;
         [Space]
-        [SerializeField] private Transform rowParent;
-        [SerializeField] private GameObject rowPrefab;
-        [SerializeField] private DialogueResponseButton dialogueResponsePrefab;
-        [Space]
-        [SerializeField] private UnityEvent onDestroy;
-
-        private bool _submitted;
+        [SerializeField] protected UnityEvent onDestroy;
         
-        private Transform RowParent => rowParent ? rowParent : transform;
+        private bool _submitted;
 
-        public void Initialize(DialogueResponseBlockNode dialogueResponseBlock, ConversationPlayer conversationUI)
+        private void OnValidate()
         {
-            if (!dialogueResponsePrefab) return;
+            if (!parent) parent = transform;
+        }
+        
+        public virtual void Initialize(DialogueResponseBlockNode dialogueResponseBlock, ConversationPlayer conversationUI)
+        {
+            if (!responseButtonPrefab || !parent) return;
             List<BasicResponse> responses = dialogueResponseBlock.GetResponses(conversationUI);
-            responses.Sort((responseX, responseY) => responseY.Row == responseX.Row ? responseX.Column - responseY.Column : responseX.Row - responseY.Row);
-            int currentRowIndex = int.MinValue;
-            float cumulativeRowHeight = 0f;
-            RectTransform currentRow = null;
+            responses.Sort((responseX, responseY) => responseY.Row == responseX.Row
+                ? responseX.Column - responseY.Column
+                : responseX.Row - responseY.Row);
             foreach (BasicResponse response in responses)
             {
                 if (response == null) continue;
-                if (!currentRow || response.Row != currentRowIndex)
-                {
-                    GameObject instantiatedRow = Instantiate(rowPrefab, RowParent);
-                    currentRow = instantiatedRow.GetComponent<RectTransform>();
-                    currentRow.anchorMin = new Vector2(xPadding, yPadding + (currentRow.GetSiblingIndex() * spacing) + cumulativeRowHeight);
-                    currentRow.anchorMax = new Vector2(1 - xPadding, yPadding + (currentRow.GetSiblingIndex() * spacing) + cumulativeRowHeight + response.Style.Height);
-                    currentRow.offsetMin = Vector2.zero;
-                    currentRow.offsetMax = Vector2.zero;
-                    currentRowIndex = response.Row;
-                    cumulativeRowHeight += response.Style.Height;
-                }
-                Instantiate(dialogueResponsePrefab, currentRow).Initialize(this, response);
+                Instantiate(responseButtonPrefab, parent).Initialize(this, response);
             }
         }
-
+        
         public void Submit(BasicResponse response)
         {
             if (_submitted) return;
@@ -61,11 +46,6 @@ namespace IronMountain.DialogueSystem.UI.Responses
         {
             onDestroy?.Invoke();
             Destroy(gameObject, destructionDelay);
-        }
-
-        private void OnValidate()
-        {
-            if (!rowParent) rowParent = transform;
         }
     }
 }
---
+++ Runtime/UI/Responses/DialogueResponseGridBlock.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using IronMountain.DialogueSystem.Nodes;
+using IronMountain.DialogueSystem.Responses;
+using UnityEngine;
+
+namespace IronMountain.DialogueSystem.UI.Responses
+{
+    public class DialogueResponseGridBlock : DialogueResponseBlock
+    {
+        [SerializeField] protected float xPadding;
+        [SerializeField] protected float yPadding;
+        [SerializeField] protected float spacing;
+        [SerializeField] protected GameObject rowPrefab;
+
+        public override void Initialize(DialogueResponseBlockNode dialogueResponseBlock, ConversationPlayer conversationUI)
+        {
+            if (!responseButtonPrefab || !parent) return;
+            List<BasicResponse> responses = dialogueResponseBlock.GetResponses(conversationUI);
+            responses.Sort((responseX, responseY) => responseY.Row == responseX.Row
+                ? responseX.Column - responseY.Column
+                : responseX.Row - responseY.Row);
+            int currentRowIndex = int.MinValue;
+            float cumulativeRowHeight = 0f;
+            RectTransform currentRow = null;
+            foreach (BasicResponse response in responses)
+            {
+                if (response == null) continue;
+                if (!currentRow || response.Row != currentRowIndex)
+                {
+                    GameObject instantiatedRow = Instantiate(rowPrefab, parent);
+                    currentRow = instantiatedRow.GetComponent<RectTransform>();
+                    currentRow.anchorMin = new Vector2(xPadding,
+                        yPadding + (currentRow.GetSiblingIndex() * spacing) + cumulativeRowHeight);
+                    currentRow.anchorMax = new Vector2(1 - xPadding,
+                        yPadding + (currentRow.GetSiblingIndex() * spacing) + cumulativeRowHeight +
+                        response.Style.Height);
+                    currentRow.offsetMin = Vector2.zero;
+                    currentRow.offsetMax = Vector2.zero;
+                    currentRowIndex = response.Row;
+                    cumulativeRowHeight += response.Style.Height;
+                }
+
+                Instantiate(responseButtonPrefab, currentRow).Initialize(this, response);
+            }
+        }
+    }
+}
